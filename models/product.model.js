a9f250f8c771bd2aea4b1cd9b12473050d383aa9
Rediat <yaiderc19@gmail.com>
2023-01-01 20:34:05 -0500

    creando modelos y rutas de los productos y usuarios

---
+++ models/product.model.js
@@ -0,0 +1,32 @@
+const {
+    Schema,
+    model
+} = require("mongoose");
+
+
+const ProductoSchema = Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es obligatorio']
+    },
+    imagen: {
+        type: String,
+        required: [true, 'La imagen es obligatoria'],
+        unique: true
+    },
+    precio: {
+        type: String,
+        required: [true, 'El precio es obligatorio']
+    },
+    calificacion: {
+        type: String
+    },
+    propietario: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Usuario",
+    },
+
+});
+
+// Se exporta el modelo de la tabla basado en el esquema anterior 
+module.exports = model('Producto', ProductoSchema);
---
+++ models/user.model.js
@@ -0,0 +1,31 @@
+const {
+    Schema,
+    model
+} = require("mongoose");
+
+
+const UsuarioSchema = Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es obligatorio']
+    },
+    correo: {
+        type: String,
+        required: [true, 'El correo es obligatorio'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'La contraseña es obligatoria']
+    }
+
+});
+
+// Se modifica el metodo que devuelve los datos de la bd con el fin de que no devuelva datos innecesarios
+UsuarioSchema.methods.toJSON = function () {
+    const {__v,password,...user} = this.toObject();
+    return user;
+}
+
+// Se exporta el modelo de la tabla basado en el esquema anterior 
+module.exports = model('Usuario', UsuarioSchema);
